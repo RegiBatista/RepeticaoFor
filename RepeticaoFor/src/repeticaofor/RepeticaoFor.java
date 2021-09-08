0668b5e965b36eb1f6d83289d45f697e536c9ed5
Reginaldo Batista <89160333+RegiBatista@users.noreply.github.com>
2021-09-08 11:29:21 -0300

    Add files via upload

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Bredd
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=0; cc<4; cc++) {
+            System.out.println("Cambalhota");
+        }
+        /*
+        int cc=o;
+        while (cc<4){
+          System.oot.println("Cambalhota");
+          cc++;
+        }
+        
+        */
+        
+    }
+    
+}
